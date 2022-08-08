b915ee3d79c07456a785720e4ddf27d6a58742be
= <hussain.alsayedali@gmail.com>
2022-08-08 22:25:05 +0300

    more challenges

---
+++ 7kyu/factorail .js
@@ -0,0 +1,7 @@
+let n = 5
+let sum = 1
+
+for(let i = 1 ; i <= n ; i++) sum *= i
+
+console.log(sum)
+return sum
---
+++ 8kyu/remove n  exclamiton mark .js
@@ -0,0 +1,8 @@
+let s = '!!!Hi!!!'
+let n = 2
+
+for(i = 0 ; i < n ; i++){
+    s = s.replace('!' , '')
+}
+console.log(s)
+return s
