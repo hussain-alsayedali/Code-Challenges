c60ed9bc140bc87ce3807781ebfd829bddff444c
= <hussain.alsayedali@gmail.com>
2022-11-07 12:23:44 +0300

    more challenges

---
+++ Javascript/7kyu/basic calculator.js
@@ -0,0 +1,10 @@
+let num1 = 1
+let num2 = 2 
+let operation = "+"
+
+
+if(operation == "+") return num1 + num2
+else if(operation == "-") return num1 - num2
+else if(operation == "*") return num1 * num2
+else if(operation == "/" && num2 != 0) return num1 / num2
+else return null
